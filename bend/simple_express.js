f4dbaa83ef9ad63a7c4d024d6e9444efb4770698
RobbieRich <robertrichardshii@gmail.com>
2019-09-13 14:26:56 -0400

    forgot

--- bend/simple_express.js
+++ bend/simple_express.js
@@ -3,26 +3,14 @@ require('dotenv').config()
 var express = require('express')
 var cors = require('cors')
 var app = express()
+const PlayerData = require('./PlayerData') 
+var port = process.env.PORT
 
-var port = 9000
-
+const playerData = new PlayerData();
 
 // as you can see from this structure the players are on hole {1, 2, 3, 4}
 const players = {
-    graph: [
-        { FirstName: "Joan", LastName: "Jet", ID: 1, Hole: 1, HoleLocation: "TEE" },
-        { FirstName: "Ruth", LastName: "Crist", ID: 2, Hole: 1, HoleLocation: "TEE" },
-        { FirstName: "Beth", LastName: "Flick", ID: 3, Hole: 1, HoleLocation: "TEE" },
-        { FirstName: "Julie", LastName: "Ant", ID: 4, Hole: 1, HoleLocation: "FWY" },
-        { FirstName: "Ginny", LastName: "Grey", ID: 5, Hole: 1, HoleLocation: "FWY" },
-        { FirstName: "Paula", LastName: "Lamb", ID: 6, Hole: 1, HoleLocation: "GRN" },
-        { FirstName: "Ingid", LastName: "Jones", ID: 7, Hole: 2, HoleLocation: "TEE" },
-        { FirstName: "Kelly", LastName: "Smith", ID: 8, Hole: 2, HoleLocation: "FWY" },
-        { FirstName: "Eilean", LastName: "Rams", ID: 9, Hole: 2, HoleLocation: "GRN" },
-        { FirstName: "Barb", LastName: "Sharp", ID: 10, Hole: 4, HoleLocation: "FWY" },
-        { FirstName: "Carol", LastName: "Adams", ID: 11, Hole: 4, HoleLocation: "FWY" },
-        { FirstName: "Faith", LastName: "Hope", ID: 12, Hole: 4, HoleLocation: "GRN" }
-    ]
+    graph: playerData.GetPlayers()
 }
 
 
